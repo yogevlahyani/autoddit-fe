96c36f5deab05c8a861308b514b0fcdfcb941131
compie <yogev@compie.co.il>
2018-06-13 09:17:41 +0300

    Delete compose

--- src/stores/index.js
+++ src/stores/index.js
@@ -4,5 +4,5 @@ import reducers from './../reducers';
 
 export default (initialState = {}) => {
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-    return createStore(reducers, initialState, composeEnhancers(applyMiddleware(ReduxThunk)));
+    return createStore(reducers, initialState, applyMiddleware(ReduxThunk));
 }
